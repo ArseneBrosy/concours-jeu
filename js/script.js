9ca0696aa57ddec931aa472967a734b9faf63f91
ArseneBrosy <arsene.brosy@divtec.ch>
2023-06-01 14:52:23 +0200

    JSON

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // Jeu de tir
 // by Arsène Brosy
+import levelsJSON from "../json/levels.json" assert {type: "json"};
+
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 
@@ -10,102 +12,26 @@ const GRAVITY_FORCE = 0.5;
 const DASH_SIZE = 125;
 const DASH_TIME = 7;
 const EFFECT_DISTANCE = 50;
-const DRAW_HITBOXES = false;
+const DRAW_HITBOXES = true;
+const DRAW_LEVEL = false;
 //#endregion
 
 //#region VARIABLES
 let canJump = false;
 let canDash = false;
-let canWallJump = false;
 let jumpSide = 1;
 let trailTime = 0;
 let dashSize = DASH_SIZE;
 let started = false;
 let ended = false;
 
-let walls = [
-    [
-        {
-            x1: 0,
-            y1: 900,
-            x2: 250,
-            y2: 1000
-        },
-        {
-            x1: 350,
-            y1: 750,
-            x2: 400,
-            y2: 850
-        },
-        {
-            x1: 200,
-            y1: 650,
-            x2: 250,
-            y2: 750
-        },
-        {
-            x1: 650,
-            y1: 700,
-            x2: 850,
-            y2: 800
-        },
-        {
-            x1: 950,
-            y1: 300,
-            x2: 1000,
-            y2: 650
-        },
-        {
-            x1: 550,
-            y1: 450,
-            x2: 650,
-            y2: 550
-        },
-        {
-            x1: 50,
-            y1: 300,
-            x2: 200,
-            y2: 350
-        },
-        {
-            x1: 0,
-            y1: 100,
-            x2: 50,
-            y2: 250
-        },
-    ]
-];
-let orbs = [
-    [
-        {
-            x: 500,
-            y: 700
-        },
-        {
-            x: 400,
-            y: 350
-        },
-    ]
-];
-let ends = [
-    {
-        x: 200,
-        y: 100
-    }
-];
-let spawns = [
-    {
-        x: 100,
-        y: 890
-    }
-];
 let levelIndex = 0;
 let trailX = 0;
 let trailY = 0;
 
 let player = {
-    x: spawns[levelIndex].x,
-    y: spawns[levelIndex].y,
+    x: levelsJSON[levelIndex].spawn.x,
+    y: levelsJSON[levelIndex].spawn.y,
     velocityX: 0,
     velocityY: 0,
     size: 20
@@ -128,35 +54,34 @@ function loop() {
     //#region COLLISONS
     let bottomDis = player.velocityY;
     canJump = false;
-    for (let i = 0; i < walls[levelIndex].length; i++) {
+    for (let i = 0; i < levelsJSON[levelIndex].walls.length; i++) {
         // bottom
-        let actualBottomDis = walls[levelIndex][i].y1 - (player.y + player.size / 2);
-        if (actualBottomDis > -player.velocityY && actualBottomDis < bottomDis && walls[levelIndex][i].x1 < player.x + player.size / 2 && walls[levelIndex][i].x2 > player.x - player.size / 2) {
+        let actualBottomDis = levelsJSON[levelIndex].walls[i].y1 - (player.y + player.size / 2);
+        if (actualBottomDis > -player.velocityY && actualBottomDis < bottomDis && levelsJSON[levelIndex].walls[i].x1 < player.x + player.size / 2 && levelsJSON[levelIndex].walls[i].x2 > player.x - player.size / 2) {
             bottomDis = actualBottomDis;
-            canMoveDown = false;
             player.velocityY = 0;
             canJump = true;
             canDash = true;
         }
         // right
-        if (player.velocityX >= 0 && player.x + player.size/2 + player.velocityX >= walls[levelIndex][i].x1 && player.x + player.size/2 <= walls[levelIndex][i].x1 && walls[levelIndex][i].y1 < player.y + player.size / 2 && walls[levelIndex][i].y2 > player.y - player.size / 2) {
-            player.x += walls[levelIndex][i].x1 - (player.x + player.size/2);
+        if (player.velocityX >= 0 && player.x + player.size/2 + player.velocityX >= levelsJSON[levelIndex].walls[i].x1 && player.x + player.size/2 <= levelsJSON[levelIndex].walls[i].x1 && levelsJSON[levelIndex].walls[i].y1 < player.y + player.size / 2 && levelsJSON[levelIndex].walls[i].y2 > player.y - player.size / 2) {
+            player.x += levelsJSON[levelIndex].walls[i].x1 - (player.x + player.size/2);
             player.velocityX = 0;
             jumpSide = -1;
             canJump = true;
             canDash = true;
         }
         // left
-        if (player.velocityX <= 0 && player.x - player.size/2 + player.velocityX <= walls[levelIndex][i].x2 && player.x - player.size/2 >= walls[levelIndex][i].x2 && walls[levelIndex][i].y1 < player.y + player.size / 2 && walls[levelIndex][i].y2 > player.y - player.size / 2) {
-            player.x -= (player.x - player.size/2) - walls[levelIndex][i].x2;
+        if (player.velocityX <= 0 && player.x - player.size/2 + player.velocityX <= levelsJSON[levelIndex].walls[i].x2 && player.x - player.size/2 >= levelsJSON[levelIndex].walls[i].x2 && levelsJSON[levelIndex].walls[i].y1 < player.y + player.size / 2 && levelsJSON[levelIndex].walls[i].y2 > player.y - player.size / 2) {
+            player.x -= (player.x - player.size/2) - levelsJSON[levelIndex].walls[i].x2;
             player.velocityX = 0;
             jumpSide = 1;
             canJump = true;
             canDash = true;
         }
     }
-    for (let i = 0; i < orbs[levelIndex].length; i++) {
-        if (Distance(orbs[levelIndex][i].x, orbs[levelIndex][i].y, player.x, player.y) <= EFFECT_DISTANCE) {
+    for (let i = 0; i < levelsJSON[levelIndex].orbs.length; i++) {
+        if (Distance(levelsJSON[levelIndex].orbs[i].x, levelsJSON[levelIndex].orbs[i].y, player.x, player.y) <= EFFECT_DISTANCE) {
             canJump = true;
             canDash = true;
         }
@@ -174,8 +99,8 @@ function loop() {
     //#region DEATH
     if (player.y > 1000) {
         player = {
-            x: spawns[levelIndex].x,
-            y: spawns[levelIndex].y,
+            x: levelsJSON[levelIndex].spawn.x,
+            y: levelsJSON[levelIndex].spawn.y,
             velocityX: 0,
             velocityY: 0,
             size: 20
@@ -186,8 +111,19 @@ function loop() {
     //#endregion
 
     //#region WIN
-    if (Distance(ends[levelIndex].x, ends[levelIndex].y, player.x, player.y) <= EFFECT_DISTANCE) {
+    if (Distance(levelsJSON[levelIndex].end.x, levelsJSON[levelIndex].end.y, player.x, player.y) <= EFFECT_DISTANCE) {
         ended = true;
+        levelIndex++;
+        player = {
+            x: levelsJSON[levelIndex].spawn.x,
+            y: levelsJSON[levelIndex].spawn.y,
+            velocityX: 0,
+            velocityY: 0,
+            size: 20
+        };
+        started = false;
+        ended = false;
+        jumpSide = 1;
     }
     //#endregion
 
@@ -197,8 +133,10 @@ function loop() {
     ctx.fillStyle = "red";
     ctx.strokeStyle = "red";
 
-    background.src = "./images/levels/level" + levelIndex + ".png";
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    if (DRAW_LEVEL) {
+        background.src = "./images/levels/level" + levelIndex + ".png";
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    }
     
     // player
     if (trailTime > 0) {
@@ -210,19 +148,19 @@ function loop() {
     
     if (DRAW_HITBOXES) {
         // walls
-        for (let i = 0; i < walls[levelIndex].length; i++) {
-            ctx.strokeRect(walls[levelIndex][i].x1, walls[levelIndex][i].y1, walls[levelIndex][i].x2 - walls[levelIndex][i].x1, walls[levelIndex][i].y2 - walls[levelIndex][i].y1);
+        for (let i = 0; i < levelsJSON[levelIndex].walls.length; i++) {
+            ctx.strokeRect(levelsJSON[levelIndex].walls[i].x1, levelsJSON[levelIndex].walls[i].y1, levelsJSON[levelIndex].walls[i].x2 - levelsJSON[levelIndex].walls[i].x1, levelsJSON[levelIndex].walls[i].y2 - levelsJSON[levelIndex].walls[i].y1);
         }
 
         // orbs
         ctx.fillStyle = "green";
-        for (let i = 0; i < orbs[levelIndex].length; i++) {
-            ctx.fillRect(orbs[levelIndex][i].x - 5, orbs[levelIndex][i].y - 5, 10, 10);
+        for (let i = 0; i < levelsJSON[levelIndex].orbs.length; i++) {
+            ctx.fillRect(levelsJSON[levelIndex].orbs[i].x - 5, levelsJSON[levelIndex].orbs[i].y - 5, 10, 10);
         }
 
         // ends
         ctx.fillStyle = "blue";
-        ctx.fillRect(ends[levelIndex].x - 5, ends[levelIndex].y - 5, 10, 10);
+        ctx.fillRect(levelsJSON[levelIndex].end.x - 5, levelsJSON[levelIndex].end.y - 5, 10, 10);
     }
     //#endregion
     requestAnimationFrame(loop);
@@ -244,16 +182,16 @@ document.addEventListener("keydown", (e) => {
             trailY = player.y - player.size/2;
             dashSize = DASH_SIZE;
             if (jumpSide === 1) {
-                for (let i = 0; i < walls[levelIndex].length; i++) {
-                    if (walls[levelIndex][i].x1 > player.x + player.size && walls[levelIndex][i].y1 < player.y + player.size / 2 && walls[levelIndex][i].y2 > player.y - player.size / 2) {
-                        let distance = walls[levelIndex][i].x1 - (player.x + player.size);
+                for (let i = 0; i < levelsJSON[levelIndex].walls.length; i++) {
+                    if (levelsJSON[levelIndex].walls[i].x1 > player.x + player.size && levelsJSON[levelIndex].walls[i].y1 < player.y + player.size / 2 && levelsJSON[levelIndex].walls[i].y2 > player.y - player.size / 2) {
+                        let distance = levelsJSON[levelIndex].walls[i].x1 - (player.x + player.size);
                         dashSize = distance < dashSize ? distance : dashSize;
                     }
                 }
             } else {
-                for (let i = 0; i < walls[levelIndex].length; i++) {
-                    if (walls[levelIndex][i].x2 < player.x - player.size && walls[levelIndex][i].y1 < player.y + player.size / 2 && walls[levelIndex][i].y2 > player.y - player.size / 2) {
-                        let distance = (player.x - player.size) - walls[levelIndex][i].x2;
+                for (let i = 0; i < levelsJSON[levelIndex].walls.length; i++) {
+                    if (levelsJSON[levelIndex].walls[i].x2 < player.x - player.size && levelsJSON[levelIndex].walls[i].y1 < player.y + player.size / 2 && levelsJSON[levelIndex].walls[i].y2 > player.y - player.size / 2) {
+                        let distance = (player.x - player.size) - levelsJSON[levelIndex].walls[i].x2;
                         dashSize = distance < dashSize ? distance : dashSize;
                     }
                 }
